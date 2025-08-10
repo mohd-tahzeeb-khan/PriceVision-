0d37b046095b64d3095ead598b0a78af2f69e450
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2025-08-11 02:24:16 +0530

    Alter the Endpoint for better performance: This update will hide the elements of id.

--- intellicore-api/src/main/java/com/datastream/intellicore_api/controller/UserController.java
+++ intellicore-api/src/main/java/com/datastream/intellicore_api/controller/UserController.java
@@ -6,12 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
     @Autowired
     private userService userservice;
 
+    @GetMapping("/getall")
+    public List<User> getAll(){
+        return userservice.Getall();
+    }
     @PostMapping("/create")
     public ResponseEntity<String> createUser(@RequestBody User user) {
         User checkuser=userservice.Storeuser(user);
