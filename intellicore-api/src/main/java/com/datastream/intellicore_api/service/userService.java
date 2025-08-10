3165b1bb15fa489768dc6ccc743ad80db36228cc
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2025-08-11 01:31:28 +0530

    Service layer file for User. This service file will be used to interect with API endpoint and Business logic.

--- intellicore-api/src/main/java/com/datastream/intellicore_api/service/userService.java
+++ intellicore-api/src/main/java/com/datastream/intellicore_api/service/userService.java
@@ -1,2 +1,22 @@
-package com.datastream.intellicore_api.service;public class userService {
+package com.datastream.intellicore_api.service;
+
+import com.datastream.intellicore_api.entity.User;
+import com.datastream.intellicore_api.repository.userRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class userService {
+    @Autowired
+    private userRespository userrespository;
+
+    public User Storeuser(User user) {
+        if (userrespository.findByEmail(user.getEmail()).isPresent()) {
+            return null;
+        }
+        return userrespository.save(user);
+    }
 }
+
