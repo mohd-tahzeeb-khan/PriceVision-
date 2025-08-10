e8fb6b4dba04314436ca9460133be82ad25a02e9
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2025-08-11 01:28:22 +0530

    Created new file in repository Named 'userRespository' which will help User Entity to interect with database and controller

--- intellicore-api/src/main/java/com/datastream/intellicore_api/repository/userRespository.java
+++ intellicore-api/src/main/java/com/datastream/intellicore_api/repository/userRespository.java
@@ -1,2 +1,14 @@
-package com.datastream.intellicore_api.repository;public interface userRespository {
+package com.datastream.intellicore_api.repository;
+
+import com.datastream.intellicore_api.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface userRespository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByPhone(String phone);
 }
