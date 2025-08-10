300b65b17517c4e5d7c914fe3103a7ecc2ade8fa
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2025-08-11 01:32:36 +0530

    Security Configuration file. This will handle all the configurations of Security in Project

--- intellicore-api/src/main/java/com/datastream/intellicore_api/config/SecurityConfig.java
+++ intellicore-api/src/main/java/com/datastream/intellicore_api/config/SecurityConfig.java
@@ -1,2 +1,18 @@
-package com.datastream.intellicore_api.config;public class SecurityConfig {
+package com.datastream.intellicore_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws
+            Exception {http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth ->auth.requestMatchers("/user/**").permitAll().anyRequest().authenticated());
+
+        return http.build();
+    }
+
 }
