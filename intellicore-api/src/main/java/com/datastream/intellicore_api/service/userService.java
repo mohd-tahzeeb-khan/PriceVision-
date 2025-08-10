3c25483e2a606b4a3f088b24e591a8704160fe1d
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2025-08-11 02:23:19 +0530

    Alter the Existing Endpoint of Getall to render the hidden data to maintain privacy

--- intellicore-api/src/main/java/com/datastream/intellicore_api/service/userService.java
+++ intellicore-api/src/main/java/com/datastream/intellicore_api/service/userService.java
@@ -2,21 +2,35 @@ package com.datastream.intellicore_api.service;
 
 import com.datastream.intellicore_api.entity.User;
 import com.datastream.intellicore_api.repository.userRespository;
+import com.datastream.intellicore_api.util.MaskingId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
 
 @Service
 public class userService {
     @Autowired
     private userRespository userrespository;
 
+
+//    private MaskingId maskingId;
+
     public User Storeuser(User user) {
         if (userrespository.findByEmail(user.getEmail()).isPresent()) {
             return null;
         }
         return userrespository.save(user);
+    }
+    public List<User> Getall() {
+        List<User> users = userrespository.findAll();
+        users.forEach(user -> {
+            String id=user.getId();
+            String alteid= MaskingId.MaskingIdUtility(id);
+            user.setId(alteid);
+
+        });
+        return users;
     }
 }
 
