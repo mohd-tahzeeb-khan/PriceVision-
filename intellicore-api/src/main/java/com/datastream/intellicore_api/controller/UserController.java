2c3a825569fcdf0e1443693a970bac148d771953
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2025-08-11 01:30:01 +0530

    Create file in Controller Named 'UserController', this file is Controller for User. More Endpoints will be added to this UserController

--- intellicore-api/src/main/java/com/datastream/intellicore_api/controller/UserController.java
+++ intellicore-api/src/main/java/com/datastream/intellicore_api/controller/UserController.java
@@ -1,2 +1,23 @@
-package com.datastream.intellicore_api.controller;public class UserController {
+package com.datastream.intellicore_api.controller;
+
+import com.datastream.intellicore_api.entity.User;
+import com.datastream.intellicore_api.service.userService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private userService userservice;
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+        User checkuser=userservice.Storeuser(user);
+        if(checkuser==null) {
+            return ResponseEntity.status(409).body("User Creation Failed. User Already Exists");
+        }
+        return ResponseEntity.ok("User Created");
+    }
 }
